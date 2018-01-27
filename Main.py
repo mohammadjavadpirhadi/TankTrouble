dd9d2116eeca3723561e1540c65767381e20136d
mohammadjavadpirhadi <mohammadjavad.pirhadi@gmail.com>
2018-01-27 16:21:18 +0330

    Handle ball collision , tank move , ball move , keys ,tank firing , ...

--- Main.py
+++ Main.py
@@ -1,38 +1,109 @@
-import pygame, sys, random, math
-import pygame.locals as GAME_GLOBALS
+import math
+import pygame
 import pygame.event as GAME_EVENTS
+import pygame.locals as GAME_GLOBALS
 import pygame.time as GAME_TIME
+import random
+import sys
+
 import Objects
 
 pygame.init()
 world = Objects.world()
 surface = pygame.display.set_mode((1280, 720), pygame.FULLSCREEN)
-greenTankPictureDirectory = 'Tanks/greenTank.png'
-greenTank = Objects.tank(30, 30, greenTankPictureDirectory)
-candyTankPictureDirectory = 'Tanks/candyTank.png'
-candyTank = Objects.tank(1175, 640, candyTankPictureDirectory)
+greenTankPictureDirectory = 'Tanks/greenTank3.png'
+greenTank = Objects.tank(50, 50, greenTankPictureDirectory)
+purpleTankPictureDirectory = 'Tanks/purpleTank.png'
+purpleTank = Objects.tank(1230, 670, purpleTankPictureDirectory)
 
 
 def quitGame():
     pygame.quit()
     sys.exit()
-    
+
 
 while True:
-    world.drawMap(surface)
-    greenTank.drawTank(surface)
-    candyTank.drawTank(surface)
-
     for event in GAME_EVENTS.get():
 
         if event.type == pygame.KEYDOWN:
-
+            # greenTank
+            if event.key == pygame.K_UP:
+                greenTank.forwardDirection = True
+                greenTank.backwardDirection = False
+            if event.key == pygame.K_DOWN:
+                greenTank.forwardDirection = False
+                greenTank.backwardDirection = True
+            if event.key == pygame.K_LEFT:
+                greenTank.leftRotate = True
+                greenTank.rightRotate = False
+            if event.key == pygame.K_RIGHT:
+                greenTank.leftRotate = False
+                greenTank.rightRotate = True
+            if event.key == pygame.K_RCTRL:
+                greenTank.fire()
+            # purpleTank
+            if event.key == pygame.K_e:
+                purpleTank.forwardDirection = True
+                purpleTank.backwardDirection = False
+            if event.key == pygame.K_d:
+                purpleTank.forwardDirection = False
+                purpleTank.backwardDirection = True
+            if event.key == pygame.K_s:
+                purpleTank.leftRotate = True
+                purpleTank.rightRotate = False
+            if event.key == pygame.K_f:
+                purpleTank.leftRotate = False
+                purpleTank.rightRotate = True
+            if event.key == pygame.K_a:
+                purpleTank.fire()
             if event.key == pygame.K_ESCAPE:
                 quitGame()
 
     if event.type == pygame.KEYUP:
-
+        # greenTank
+        if event.key == pygame.K_UP:
+            greenTank.forwardDirection = False
+        if event.key == pygame.K_DOWN:
+            greenTank.backwardDirection = False
+        if event.key == pygame.K_LEFT:
+            greenTank.leftRotate = False
+        if event.key == pygame.K_RIGHT:
+            greenTank.rightRotate = False
+        # purpleTank
+        if event.key == pygame.K_e:
+            purpleTank.forwardDirection = False
+        if event.key == pygame.K_d:
+            purpleTank.backwardDirection = False
+        if event.key == pygame.K_s:
+            purpleTank.leftRotate = False
+        if event.key == pygame.K_f:
+            purpleTank.rightRotate = False
         if event.type == GAME_GLOBALS.QUIT:
             quitGame()
 
+    world.drawMap(surface)
+    greenTank.move()
+    greenTank.rotate()
+    greenTank.drawTank(surface)
+    purpleTank.move()
+    purpleTank.rotate()
+    purpleTank.drawTank(surface)
+    for bullet in purpleTank.bullets:
+        collisionKind = bullet.collision(surface)
+        if collisionKind == "GREEN TANK COLLISION":
+            greenTank.isWracked = True
+            bullet.isExpired = True
+        elif collisionKind == "PURPLE TANK COLLISION":
+            purpleTank.isWracked = True
+            bullet.isExpired = True
+        bullet.draw(surface)
+    for bullet in greenTank.bullets:
+        collisionKind = bullet.collision(surface)
+        if collisionKind == "GREEN TANK COLLISION":
+            greenTank.isWracked = True
+            bullet.isExpired = True
+        elif collisionKind == "PURPLE TANK COLLISION":
+            purpleTank.isWracked = True
+            bullet.isExpired = True
+        bullet.draw(surface)
     pygame.display.update()
--- Objects.py
+++ Objects.py
@@ -1,9 +1,14 @@
-import random, pygame
+import math
+import pygame
+import pygame.event as GAME_EVENTS
+import pygame.locals as GAME_GLOBALS
+import pygame.time as GAME_TIME
+import random
 
 
 class world:
     def loadMaps(self):
-        self.maps.append(pygame.image.load('Maps/Map.png'))
+        self.maps.append(pygame.image.load('Maps/Map1.png'))
 
     def chooseMap(self):
         self.Map = self.maps[random.randrange(len(self.maps))]
@@ -17,23 +22,137 @@ class world:
         self.loadMaps()
         self.Map = None
 
-
 class tank:
 
-    def loadPicture(self, directory):
-        self.picture = pygame.image.load(directory)
+    def loadPicture(self, Imagedirectory):
+        self.picture = pygame.image.load(Imagedirectory)
+
+    def rotate(self):
+        if self.leftRotate:
+            self.angle += self.rotateSpeed
+            if self.angle >= 360:
+                self.angle = self.angle - (360 * int(self.angle / 360))
+        if self.rightRotate:
+            self.angle -= self.rotateSpeed
+            if self.angle <= -360:
+                self.angle = self.angle + (360 * int(self.angle / -360))
+        self.rouPicture = pygame.transform.rotate(self.picture, self.angle)
 
     def drawTank(self, surface):
-        surface.blit(self.picture, (self.x, self.y))
+        if not self.isWracked:
+            halfOfDiameter = (self.pictureWidth ** 2 + self.pictureWidth ** 2) ** (0.5)
+            self.pictureAngle = self.angle
+            if self.pictureAngle >= 90:
+                self.pictureAngle = self.pictureAngle - (90 * int(self.angle / 90))
+            if self.pictureAngle <= -90:
+                self.pictureAngle = self.pictureAngle + (90 * int(self.angle / -90))
+            if self.pictureAngle < 0:
+                self.pictureAngle = -self.pictureAngle
+            xyDifference = (self.pictureWidth * math.cos(
+                (self.pictureAngle) * math.pi / 180) + self.pictureWidth * math.sin(
+                (self.pictureAngle) * math.pi / 180))
 
-    def __init__(self, X, Y, directory):
-        self.picture = None
-        self.loadPicture(directory)
+            surface.blit(self.rouPicture,
+                         (
+                             self.x - xyDifference / 2,
+                             self.y - xyDifference / 2))
+
+    def move(self):
+        if self.forwardDirection:
+            self.vx = self.v * math.cos((-self.angle * math.pi) / 180)
+            self.vy = self.v * math.sin((-self.angle * math.pi) / 180)
+            self.x += self.vx
+            self.y += self.vy
+        if self.backwardDirection:
+            self.vx = self.v * math.cos((-self.angle * math.pi) / 180)
+            self.vy = self.v * math.sin((-self.angle * math.pi) / 180)
+            self.x -= self.vx
+            self.y -= self.vy
+
+    def fire(self):
+        if not self.isWracked and len(self.bullets) < 5:
+            bullet(self)
+
+    def collision(self):
+        pass
+
+    def __init__(self, X, Y, Imagedirectory):
         self.x = X
         self.y = Y
+        self.picture = None
+        self.rouPicture = None
+        self.pictureAngle = 0
+        self.loadPicture(Imagedirectory)
         self.height = 50
         self.width = 75
+        self.pictureHeight = 75
+        self.pictureWidth = 75
+        self.angle = random.randrange(360)
+        self.rotateSpeed = 3
+        self.v = 5
+        self.vx = self.v * math.cos((-self.angle * math.pi) / 180)
+        self.vy = self.v * math.sin((-self.angle * math.pi) / 180)
+        self.forwardDirection = False
+        self.backwardDirection = False
+        self.leftRotate = False
+        self.rightRotate = False
+        self.rotate()
+        self.bullets = []
+        self.isWracked = False
 
 
 class bullet:
-    pass
+    def __init__(self, tank):
+        self.tank = tank
+        self.x = tank.x + tank.width * math.cos((-tank.angle * math.pi) / 180) / 2
+        self.y = tank.y + tank.width * math.sin((-tank.angle * math.pi) / 180) / 2
+        self.angle = tank.angle
+        self.radius = 5
+        self.v = 3.5
+        self.vx = self.v * math.cos((self.angle * math.pi) / 180)
+        self.vy = self.v * math.sin((self.angle * math.pi) / 180)
+        tank.bullets.append(self)
+        self.isExpired = False
+        self.startTime = GAME_TIME.get_ticks()
+        self.expireTime = 10000
+
+    def draw(self, surface):
+        if not self.isExpired:
+            if (GAME_TIME.get_ticks() - self.startTime) < self.expireTime:
+                self.move()
+                pygame.draw.circle(surface, (0, 0, 0), (round(self.x), round(self.y)), self.radius, 0)
+            else:
+                self.isExpired = True
+                self.tank.bullets.remove(self)
+
+    def move(self):
+        self.vx = self.v * math.cos((-self.angle * math.pi) / 180)
+        self.vy = self.v * math.sin((-self.angle * math.pi) / 180)
+        self.x += self.vx
+        self.y += self.vy
+
+    def collision(self, surface):
+        if surface.get_at((int(self.x + self.radius), int(self.y))) == (101, 101, 101, 255):
+            self.angle = 180 - self.angle
+        elif surface.get_at((int(self.x - self.radius), int(self.y))) == (101, 101, 101, 255):
+            self.angle = 180 - self.angle
+        elif surface.get_at((int(self.x), int(self.y + self.radius))) == (101, 101, 101, 255):
+            self.angle = - self.angle
+        elif surface.get_at((int(self.x), int(self.y - self.radius))) == (101, 101, 101, 255):
+            self.angle = - self.angle
+            return "WALL COLLISION"
+        if surface.get_at((int(self.x + self.radius), int(self.y))) == (1, 1, 1, 255) or surface.get_at(
+                (int(self.x - self.radius), int(self.y))) == (1, 1, 1, 255) or surface.get_at(
+                (int(self.x), int(self.y + self.radius))) == (1, 1, 1, 255) or surface.get_at(
+                (int(self.x), int(self.y - self.radius))) == (1, 1, 1, 255):
+            if GAME_TIME.get_ticks() - self.startTime > 100:
+                return "PURPLE TANK COLLISION"
+        if surface.get_at((int(self.x + self.radius), int(self.y))) == (1, 101, 1, 255) or surface.get_at(
+                (int(self.x - self.radius), int(self.y))) == (1, 101, 1, 255) or surface.get_at(
+                (int(self.x), int(self.y + self.radius))) == (1, 101, 1, 255) or surface.get_at(
+                (int(self.x), int(self.y - self.radius))) == (1, 101, 1, 255):
+            if GAME_TIME.get_ticks() - self.startTime > 100:
+                return "GREEN TANK COLLISION"
+    # def collisions(self):
+    # if surface.get_at(self.x , self.y + self.radius) == (0, 0, 0, 255):
+    # leftOfPlayerOnPlatform = False
